83ceca9a518bfe623cc73902108fd18407b7907f
Mojtaba Gharibreza <mojtabaqri@gmail.com>
2020-07-19 20:23:55 +0430

    created

---
+++ bot.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'methods.php';
+require_once 'config.php';
+////end require
+//$data=json_decode(file_get_contents("php://input"));
+//$fullMessage=$data->message; //get fullmesage
+//
+////Check User is admin
+//if($fullMessage->from->id!=admin){
+//    SendMessage($fullMessage->from->id,"شما مدیر نیستید! دسترسی برای شما محدود شده !");
+//    exit();
+//}
+////end Check
+////if message has attachment $attachment is true ---------------------
+
+//----------------------------------------------------------------------------------------------------------
+
+var_dump(connect());
+
---
+++ methods.php
@@ -0,0 +1,81 @@
+<?php
+require_once 'config.php';
+function bot($method, $datas = [])
+{
+    $url = "https://api.telegram.org/bot" . apiKey . "/" . $method;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($datas));
+    $res = curl_exec($ch);
+    if (curl_error($ch))
+    {
+        var_dump(curl_error($ch));
+    }
+    else
+    {
+        return json_decode($res);
+    }
+}
+
+
+function connect(){
+    $headers = array(
+        'Accept: application/json',
+        'Content-Type: application/json',
+    );
+    $cURLConnection = curl_init();
+    curl_setopt($cURLConnection, CURLOPT_URL, 'https://www.navasan.net/last_currencies.php');
+    curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($cURLConnection, CURLOPT_HTTPHEADER, $headers);
+    $body = curl_exec($cURLConnection);
+    curl_close($cURLConnection);
+    $jsonArrayResponse = json_decode($body);
+    return $jsonArrayResponse;
+}
+//-----------------------------------------methods-------------------------------------------------------------
+
+function SendMessage($chat_id, $text){
+    bot('sendMessage',[
+        'chat_id'=>$chat_id,
+        'text'=>$text,
+        'parse_mode'=>'MarkDown']);
+}
+function sendphoto($chat_id, $photo, $captionl){
+    bot('sendphoto',[
+        'chat_id'=>$chat_id,
+        'photo'=>$photo,
+        'caption'=>$captionl,
+    ]);
+}
+function sendaudio($chat_id, $audio, $caption, $title ,$performer){
+    bot('sendaudio',[
+        'chat_id'=>$chat_id,
+        'audio'=>$audio,
+        'caption'=>$caption,
+        'title'=>$title,
+        'performer'=>$performer
+    ]);
+}
+function sendDocument($chat_id, $document, $caption=null){
+    bot('senddocument',[
+        'chat_id'=>$chat_id,
+        'document'=>$document,
+        'caption'=>$caption
+    ]);
+}
+
+function sendvideo($chat_id, $video, $caption){
+    bot('sendvideo',[
+        'chat_id'=>$chat_id,
+        'video'=>$video,
+        'caption'=>$caption
+    ]);
+}
+function sendvoice($chat_id, $voice, $caption){
+    bot('sendvoice',[
+        'chat_id'=>$chat_id,
+        'voice'=>$voice,
+        'caption'=>$caption
+    ]);
+}
